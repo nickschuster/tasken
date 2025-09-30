c1c3721f7088fa8c9d3cdbb37c78b06150b4428b
Nick Schuster <36465360+nickschuster@users.noreply.github.com>
2025-09-29 17:19:51 -0700

    chore(meta): favicon and title (#6)
    - update favicon and meta tags for title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
-	
+
 	let { children } = $props();
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	<title>Tasken</title>
 </svelte:head>
 
 {@render children?.()}
