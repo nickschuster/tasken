ec485c03793552f2916fb5a65d5b639a9e107b20
Nick Schuster <ngpnickschuster@gmail.com>
2025-10-01 15:14:10 -0700

    log

--- src/lib/server/db/migrate.ts
+++ src/lib/server/db/migrate.ts
@@ -5,6 +5,8 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+console.log('Starting migrations...', process.env.DATABASE_URL);
+
 if (!process.env.DATABASE_URL) {
 	throw new Error('DATABASE_URL is not set');
 }
