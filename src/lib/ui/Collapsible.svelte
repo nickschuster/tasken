0586fc2f2b1e7cc0ddd4514db8ad16178c75a4fb
Tom Schuster <100451897+TomSchuu@users.noreply.github.com>
2025-10-01 14:59:07 -0700

    feat(tasks): add completed tasks list  (#8)
    - added expandable completed tasks list below main tasks

---
+++ src/lib/ui/Collapsible.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { ChevronDown } from '@lucide/svelte';
+	import { slide } from 'svelte/transition';
+
+	export let headerText: string = '';
+
+	let isOpen: boolean = false;
+</script>
+
+<div>
+	<h2>
+		<button
+			on:click={() => (isOpen = !isOpen)}
+			class="flex w-full cursor-pointer justify-between border-none bg-white p-2 text-neutral-800 dark:bg-black dark:text-neutral-200"
+		>
+			{headerText}
+			<ChevronDown class="transition-transform duration-100 {isOpen ? 'rotate-180' : ''}" />
+		</button>
+	</h2>
+	{#if isOpen}
+		<div class="content" transition:slide={{ duration: 100 }}>
+			<slot></slot>
+		</div>
+	{/if}
+</div>
