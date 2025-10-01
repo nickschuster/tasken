79fbad1fc5fc21a1ad34888fa0652a31eb766679
Nick Schuster <ngpnickschuster@gmail.com>
2025-10-01 15:42:03 -0700

    try smooth

--- src/lib/server/db/migrate.ts
+++ src/lib/server/db/migrate.ts
@@ -5,8 +5,6 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-console.log('Starting migrations...', process.env.DATABASE_URL?.split('/'));
-
 if (!process.env.DATABASE_URL) {
 	throw new Error('DATABASE_URL is not set');
 }
