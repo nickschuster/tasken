ba2a238054968d562b71b6d4b1cbb718139a4062
Nick Schuster <ngpnickschuster@gmail.com>
2025-10-17 11:42:24 -0700

    feat(tasks): new check animation

--- src/lib/ui/task/TaskCheck.svelte
+++ src/lib/ui/task/TaskCheck.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
+	import { Check, Circle, CircleCheckBigIcon } from '@lucide/svelte';
+
 	export let checked = false;
 	export let toggleChecked: (checked: boolean) => void;
 
+	let mouseDown = false;
 	let visuallyChecked = checked;
-	const ANIMATION_DURATION = 250; // ms
+	const ANIMATION_DURATION_MS = 200;
 
 	function handleToggle(event: Event) {
 		event.preventDefault();
@@ -13,7 +16,7 @@
 
 		setTimeout(() => {
 			toggleChecked(visuallyChecked);
-		}, ANIMATION_DURATION);
+		}, ANIMATION_DURATION_MS);
 	}
 
 	const handleKeydown = (event: KeyboardEvent) => {
@@ -26,90 +29,59 @@
 </script>
 
 <button
+	class="relative cursor-pointer rounded-full hover:bg-neutral-200 dark:hover:bg-neutral-950"
 	role="checkbox"
 	aria-label="checkbox"
 	aria-checked={visuallyChecked}
-	class="checkbox-container"
 	class:checked={visuallyChecked}
+	on:mousedown={() => (mouseDown = true)}
+	on:mouseup={() => (mouseDown = false)}
+	on:mouseleave={() => (mouseDown = false)}
 	on:click={handleToggle}
 	on:keydown={handleKeydown}
 	title={visuallyChecked ? 'Uncheck' : 'Check'}
 >
-	<svg class="checkmark" viewBox="0 0 24 24">
-		<path d="M4.5 12.75l6 6 9-13.5" stroke-linecap="round" stroke-linejoin="round" />
-	</svg>
+	<div
+		class="check-div aspect-square h-[28] w-[28px] rounded-full {visuallyChecked || mouseDown
+			? 'bg-black'
+			: ''}"
+	>
+		<Circle
+			size="28"
+			class="circle absolute rounded-full text-black dark:text-neutral-800 {visuallyChecked ||
+			mouseDown
+				? 'opacity-0'
+				: ''}"
+		/>
+		<Check
+			strokeWidth="3"
+			size="20"
+			class="circle-check absolute translate-1 rounded-full   {visuallyChecked || mouseDown
+				? 'text-white opacity-100'
+				: 'text-black opacity-0'}"
+		/>
+	</div>
 </button>
 
 <style>
-	.checkbox-container {
-		--size: 28px;
-		--primary-color: #000000;
-		--secondary-color: #ffffff;
-		--border-color: #cccccc;
-		--border-hover-color: #999999;
-
-		aspect-ratio: 1;
-		display: inline-flex;
-		align-items: center;
-		justify-content: center;
-		width: var(--size);
-		height: var(--size);
-		padding: 0;
-		margin: 0;
-		cursor: pointer;
-		background-color: var(--secondary-color);
-		border: 2px solid var(--border-color);
-		border-radius: 50%;
-		-webkit-tap-highlight-color: transparent;
-
-		transition:
-			transform 0.15s cubic-bezier(0.34, 1.56, 0.64, 1),
-			background-color 0.15s ease-in-out,
-			border-color 0.15s ease-in-out,
-			box-shadow 0.15s ease-in-out;
+	:global(.check-div:hover .circle-check) {
+		opacity: 100%;
+		transform: scale(100%);
 	}
 
-	.checkbox-container:focus-visible {
-		outline: 2px solid var(--primary-color);
-		outline-offset: 3px;
+	.checked {
+		animation: pop 0.2s cubic-bezier(0.175, 0.885, 0.32, 1.275);
 	}
 
-	.checkbox-container:not(.checked):hover {
-		border-color: var(--border-hover-color);
-		transform: scale(1.05);
-	}
-
-	.checkbox-container:active {
-		transition-duration: 0.05s;
-	}
-
-	.checkbox-container.checked {
-		background-color: var(--primary-color);
-		border-color: var(--primary-color);
-	}
-
-	.checkbox-container:hover {
-		box-shadow: inset 0 0 0 100vw rgba(0, 0, 0, 0.1);
-	}
-
-	.checkbox-container.checked:hover {
-		background-color: var(--primary-color);
-		box-shadow: inset 0 0 0 100vw rgba(255, 255, 255, 0.15);
-	}
-
-	.checkmark {
-		width: 85%;
-		height: 85%;
-		stroke: transparent;
-		stroke-width: 3.5;
-		fill: none;
-		stroke-dasharray: 24;
-		stroke-dashoffset: 24;
-		transition: stroke-dashoffset 0.2s cubic-bezier(0.34, 1.56, 0.64, 1);
-	}
-
-	.checkbox-container.checked .checkmark {
-		stroke: var(--secondary-color);
-		stroke-dashoffset: 0;
+	@keyframes pop {
+		0% {
+			transform: scale(1);
+		}
+		50% {
+			transform: scale(1.1);
+		}
+		100% {
+			transform: scale(1);
+		}
 	}
 </style>
