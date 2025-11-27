cad031fe0a3cbd61de096211d67ff0f1b00fb460
Nick Schuster <ngpnickschuster@gmail.com>
2025-11-26 16:55:53 -0800

    Revert "log(posthog): add detailed debug logging (#35)"
    This reverts commit dcc3aa72f94f59a23c0cdcf98dc4af0a23524d38.

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -5,7 +5,6 @@ const handleAuth: Handle = async ({ event, resolve }) => {
 	const { pathname } = event.url;
 
 	if (pathname.includes('relay-5HTB')) {
-		console.log('[Auth Hook] Pathname included relay-5HTB, building posthog response relay...');
 		return buildPostHogRelayResponse(pathname, event);
 	}
 
@@ -37,8 +36,6 @@ async function buildPostHogRelayResponse(pathname: string, event: RequestEvent) 
 		? 'us-assets.i.posthog.com' // change us to eu for EU Cloud
 		: 'us.i.posthog.com'; // change us to eu for EU Cloud
 
-	console.log(`[ResponseRelay] Selected posthog hostname: ${hostname}`);
-
 	// Build external URL
 	const url = new URL(event.request.url);
 	url.protocol = 'https:';
@@ -46,15 +43,10 @@ async function buildPostHogRelayResponse(pathname: string, event: RequestEvent) 
 	url.port = '443';
 	url.pathname = pathname.replace('/relay-5HTB/', '');
 
-	console.log(`[ResponseRelay] Built external URL: ${url}`);
-	console.log(`[ResponseRelay] Final proxied path: ${url.pathname}`);
-
 	// Clone and adjust headers
 	const headers = new Headers(event.request.headers);
 	headers.set('Accept-Encoding', '');
 	headers.set('host', hostname);
-
-	console.log(`[ResponseRelay] Proxying request to external host`);
 
 	// Proxy the request to the external host
 	const response = await fetch(url.toString(), {
@@ -64,14 +56,6 @@ async function buildPostHogRelayResponse(pathname: string, event: RequestEvent) 
 		duplex: 'half'
 	} as RequestInit);
 
-	console.log(`[ResponseRelay] PostHog responded with ${response.status} ${response.statusText}`);
-
-	if (!response.ok) {
-		const txt = await response.text();
-		console.log(`[ResponseRelay] Error body: ${txt}`);
-	}
-
-	console.log('[ResponseRelay] Successfully created response. Returning...');
 	return response;
 }
 
