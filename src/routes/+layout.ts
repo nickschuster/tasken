f020cc88a029206210090129b4d24230e5c58314
Nick Schuster <36465360+nickschuster@users.noreply.github.com>
2025-11-26 16:59:00 -0800

    fix: undo all logging changes (#36)

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,16 +1,9 @@
 import posthog from 'posthog-js';
 import { browser } from '$app/environment';
-import {
-	PUBLIC_POSTHOG_KEY as POSTHOG_KEY,
-	PUBLIC_CI as CI,
-	PUBLIC_DEV as DEV
-} from '$env/static/public';
+import { PUBLIC_POSTHOG_KEY as POSTHOG_KEY, PUBLIC_CI as CI, PUBLIC_DEV as DEV } from '$env/static/public';
 
 export function _initTelemetry() {
-	const isCI = CI === 'true';
-	const isDev = DEV === 'true';
-
-	if (!browser || !POSTHOG_KEY || isCI || isDev) {
+	if (!browser || !POSTHOG_KEY || CI || DEV) {
 		return;
 	}
 
@@ -19,12 +12,6 @@ export function _initTelemetry() {
 		ui_host: 'https://us.posthog.com',
 		person_profiles: 'always',
 		persistence: 'localStorage'
-	});
-
-	console.log('[InitTelementry] Success! Posthog Initialized');
-
-	posthog.on('eventCaptured', (evt) => {
-		console.log('[PostHog Debug] Event fired on client:', evt);
 	});
 }
 
