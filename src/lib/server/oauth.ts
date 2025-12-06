d85526209c90fa100347fe1906654141fc79f5b6
Nick Schuster <36465360+nickschuster@users.noreply.github.com>
2025-12-06 13:52:33 -0800

    feat(auth): google and github sso support (#43)
    - add sso integration for google and github through OAuth

---
+++ src/lib/server/oauth.ts
@@ -0,0 +1,28 @@
+import { Google, GitHub, Apple } from 'arctic';
+import {
+	GOOGLE_OAUTH_CLIENT_ID,
+	GOOGLE_OAUTH_CLIENT_SECRET,
+	GITHUB_OAUTH_CLIENT_ID,
+	GITHUB_OAUTH_CLIENT_SECRET,
+	BASE_URL
+} from '$env/static/private';
+
+const GOOGLE_REDIRECT_URI = `${BASE_URL}/api/auth/google/callback`;
+const APPLE_REDIRECT_URI = `${BASE_URL}/api/auth/apple/callback`;
+
+export const googleOAuth = new Google(
+	GOOGLE_OAUTH_CLIENT_ID,
+	GOOGLE_OAUTH_CLIENT_SECRET,
+	GOOGLE_REDIRECT_URI
+);
+
+export const githubOAuth = new GitHub(GITHUB_OAUTH_CLIENT_ID, GITHUB_OAUTH_CLIENT_SECRET, null);
+
+// export const appleOAuth = new Apple({
+// 	clientId: process.env.APPLE_OAUTH_CLIENT_ID!,
+// 	teamId: process.env.APPLE_OAUTH_TEAM_ID!,
+// 	keyId: process.env.APPLE_OAUTH_KEY_ID!,
+// 	privateKey: process.env.APPLE_OAUTH_PRIVATE_KEY!.replace(/\\n/g, '\n'),
+// 	redirectUri: process.env.APPLE_OAUTH_REDIRECT_URI!,
+// 	scopes: ['name', 'email']
+// });
---
+++ src/routes/api/auth/github/+server.ts
@@ -0,0 +1,22 @@
+import { generateState } from 'arctic';
+import type { RequestEvent } from './$types';
+import { githubOAuth } from '$lib/server/oauth';
+
+export async function GET(event: RequestEvent) {
+	const state = generateState();
+	const authorizationUrl = githubOAuth.createAuthorizationURL(state, ['read:user', 'user:email']);
+
+	event.cookies.set('github_oauth_state', state, {
+		path: '/',
+		httpOnly: true,
+		maxAge: 300, // 5 minutes
+		sameSite: 'lax'
+	});
+
+	return new Response(null, {
+		status: 302,
+		headers: {
+			Location: authorizationUrl.toString()
+		}
+	});
+}
